38a64b4b01dae97a2d7457ff29dade382aa27447
viethoang1520 <viethoang1520.dev@gmail.com>
2025-05-02 15:43:04 +0000

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
+import Login from "./pages/Login";
 import routes from "tempo-routes";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
       </>
